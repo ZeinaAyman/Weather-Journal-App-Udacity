b7a6cc217a94858fa3a899c1b9d7e0cbabe1df57
zeina <zeina.ayman@hotmail.com>
2021-06-09 17:28:43 +0200

    .

--- website/app.js
+++ website/app.js
@@ -2,7 +2,6 @@
 
 //API call provided in email: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d80f65f935fee4f923369e08455eda21
 
-const keyAPI = 'd80f65f935fee4f923369e08455eda21';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();

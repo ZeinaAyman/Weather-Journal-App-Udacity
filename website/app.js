32ce9bcf75d7fd737d392d0c248626772d556b11
zeina <zeina.ayman@hotmail.com>
2021-06-09 19:07:05 +0200

    API data and response fetch

--- website/app.js
+++ website/app.js
@@ -3,18 +3,20 @@
 //API call provided in email: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d80f65f935fee4f923369e08455eda21
 const keyAPI = 'd80f65f935fee4f923369e08455eda21';
 
-const generate = document.getElementById("#generate");
+const generate = document.getElementById("generate");
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-generate.addEventListener("click", ()=>{
+generate.addEventListener("click", async ()=>{
   const zipCode = document.getElementById("zip").value;
 
-  if(zipCode.value == ''){
+  if(zipCode == ''){
     alert("Error. Zip Code is empty!");
   }
+
+
+  const apiRes = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=' + keyAPI)
+  const data = await apiRes.json();
+  console.log(data);
 });
-
-
-fetch('http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${keyAPI}$units=metric')
